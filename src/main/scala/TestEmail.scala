b2adae1c8237a0273f30c436a7d569ebb735b88b
BYCKILIE <tudorovidiub@gmail.com>
2026-01-20 00:47:10 +0200

    up

--- src/main/scala/TestEmail.scala
+++ src/main/scala/TestEmail.scala
@@ -7,19 +7,19 @@ import config.Logging
 import email.EmailSenders
 import email.GmailSender
 
-object TestEmail extends IOApp with Logging {
+object TestEmail {
 
-  override def run(args: List[String]): IO[ExitCode] =
-    for {
-      cfg <- ConfigUtils.loadAndParse[AppConfig]("application.conf", "application")
-      _ <- logger.info("[TestEmail] Loading Gmail configuration...")
-      gmailSender = GmailSender(cfg.gmailConfig)
-      _ <- logger.info("[TestEmail] Sending test welcome email...")
-      _ <- EmailSenders.sendWelcome(
-        gmailSender = gmailSender,
-        email = "tudor.tdr.7@gmail.com",
-        name = "Test User"
-      )
-      _ <- logger.info("[TestEmail] Test email sent successfully!")
-    } yield ExitCode.Success
+//  override def run(args: List[String]): IO[ExitCode] =
+//    for {
+//      cfg <- ConfigUtils.loadAndParse[AppConfig]("application.conf", "application")
+//      _ <- logger.info("[TestEmail] Loading Gmail configuration...")
+//      gmailSender = GmailSender(cfg.gmailConfig)
+//      _ <- logger.info("[TestEmail] Sending test welcome email...")
+//      _ <- EmailSenders.sendWelcome(
+//        gmailSender = gmailSender,
+//        email = "tudor.tdr.7@gmail.com",
+//        name = "Test User"
+//      )
+//      _ <- logger.info("[TestEmail] Test email sent successfully!")
+//    } yield ExitCode.Success
 }
